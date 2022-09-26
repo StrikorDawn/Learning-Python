95a4d8f023853e730d0ab9f5985b59fa74655161
StrikorDawnh <tadawg007@byui.edu>
2022-09-26 12:10:40 -0600

    Numbers

---
+++ In Class/numbers.py
@@ -0,0 +1,34 @@
+# #Strings are text based variables
+# #numbers are devided into 2 different ways
+# #intigers which are whole numbers
+# #float leads to partal
+# #boolean = true/false
+one = "1"
+one_num = 1
+# print(one)
+# print(one_num)
+
+# print(one + one)
+# print(one_num + one_num)
+# print(one + str(one_num)) #be carful with using this method of "fixing code"
+# print(int(one) + one_num) #be carful with using this method of "fixing code"
+print(f'{one} + {one_num}')
+
+# num_customers = input('How many customers are there? ')
+# print(one_num + int(num_customers))
+
+# one = 10
+# two= 3
+
+# print(one + two)
+# print(one - two)
+# print(one * two)
+# print(one / two)
+# print(one ** two) #multiplying by the power of (Exponents)
+# print(one // two) #discards the remander
+# print(one % two) #Just gives you the remander
+
+print(1%2)
+print(2%2) #Can use to detect if numbers are even or odd
+
+
