21f31fcbbcb361527b45a765bc185f4400078a66
StrikorDawnh <tadawg007@byui.edu>
2022-11-07 09:40:06 -0700

    Wordle completed

--- Assignments/week 8/Team8.py
+++ Assignments/week 8/Team8.py
@@ -15,19 +15,19 @@ first_name = 'Brigham'
 for i, letter in enumerate(first_name):
     print(f"The letter {letter} is at position {i}")
 
-# yes = True
+yes = True
 
-# while yes:
+while yes:
 
-#     quote = "In coming days, it will not be possible to survive spiritually without the guiding, directing, comforting, and constant influence of the Holy Ghost."
+    quote = "In coming days, it will not be possible to survive spiritually without the guiding, directing, comforting, and constant influence of the Holy Ghost."
 
-#     num = int(input("Please enter a number: " ))
+    num = int(input("Please enter a number: " ))
 
-#     for i, letter in enumerate(quote):
-#         if i % num == 0:
-#             letter = letter.upper()
-#         print(letter,end="")
-#     print()
-#     try_again = input('Would you like try again?(Yes/No) ')
-#     if try_again != yes:
-#         yes = False
+    for i, letter in enumerate(quote):
+        if i % num == 0:
+            letter = letter.upper()
+        print(letter,end="")
+    print()
+    try_again = input('Would you like try again?(Yes/No) ')
+    if try_again != yes:
+        yes = False
--- Assignments/week 8/wordle2.py
+++ Assignments/week 8/wordle2.py
@@ -1,20 +1,54 @@
-word = "reach"
+import random
+word_list = ["reach",'space','cheese','goat','steak', 'banana','chief']
+select = random.randint(0, len(word_list) - 1)
+word = word_list[select]
 play = True
 secret = '_ '*len(word)
 guess_count = 1
 
 print(f"The word is: {secret}")
+
 while play:
     guess = input("What is your guess? ")
+    if len(guess) != len(word):
+        print("Your guess must be the same length as the word.")
+    elif guess != word:
+        hint = ""
+        for i, g_letter in enumerate(guess):
+            is_in_word = False
+            is_in_pos = False
+            for j, s_letter in enumerate(word):
+            
+                if s_letter.lower() == g_letter.lower():
+                    is_in_word = True
+                    if i == j:
+                        is_in_pos = True
 
-    for i in enumerate(word):
-        for a, g_letter in enumerate(guess):
-            if guess != word:
-                print('Guess again')
-                print(f"Your hint is: {secret}")
-                guess = input('What is the word? ').lower()
-                guess_count = guess_count + 1
+            if is_in_word and is_in_pos:
+                hint += g_letter.upper()
+            elif is_in_word:
+                hint += g_letter.lower()
             else:
-                print("Guess must be same length as word.")
-
-print(f"That's Correct! You got it in {guess_count} guesses!")
+                hint += "_"
+            hint += " "
+        print(f"Your hint is {hint}")
+        guess_count = guess_count + 1
+    else:
+        print(f"Congradulations! You guessed correct in {guess_count}!")
+        invalid = True
+        while invalid:  
+            again = input("Do you want to play again? ")
+            if again.lower() == 'no':
+                play = False
+                invalid = False
+            elif again.lower() == 'yes':
+                play = True
+                select = random.randint(0, len(word_list) - 1)
+                word = word_list[select]
+                secret = '_ '*len(word)
+                guess_count = 1
+                print(f"The word is: {secret}")
+                invalid = False
+            else:
+                print('Invalid input must be yes or no.')
+            
